da480e6f269d20138f58e07e04125bdedcbf42ab
Natalia Mateo <nmateo7@gmail.com>
2020-05-25 11:19:14 +0200

    Update docs

---
+++ docs/precache-manifest.f28790e502c46dd7210c9a89d799e050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d59fdc622a3858180b7a8adb8ad110",
+    "url": "./index.html"
+  },
+  {
+    "revision": "134b8adee19896cb4473",
+    "url": "./static/css/main.63d10779.chunk.css"
+  },
+  {
+    "revision": "a6863d6d4d54ac92d5af",
+    "url": "./static/js/2.c44646d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c44646d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134b8adee19896cb4473",
+    "url": "./static/js/main.48157887.chunk.js"
+  },
+  {
+    "revision": "cbb7502546e28b5dbd02",
+    "url": "./static/js/runtime-main.5c2b7656.js"
+  }
+]);
