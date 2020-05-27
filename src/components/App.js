8e220d16e86a1055ab5e476340b9c814c46564de
Natalia Mateo <nmateo7@gmail.com>
2020-05-27 21:01:24 +0200

    Create title styles

--- src/components/App.js
+++ src/components/App.js
@@ -37,7 +37,9 @@ const App = () => {
 
   return (
     <>
-      <h1 className='title'>Mini-puzzle</h1>
+      <h1 className='title'>
+        Mini-puzzle <i className='fas fa-puzzle-piece'></i>
+      </h1>
       <div className='App'>
         <Board id='puzzle-1' pieces={leftPuzzle} puzzleState={puzzleState} />
         <Board id='puzzle-2' pretty={true} pieces={rightPuzzle} puzzleState={puzzleState} />
